f923546fe50adfe55ae6014544acc04524cc268d
Diogo Ide <diogoide@gmail.com>
2018-04-05 00:01:45 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length 
+    i += 1
+  end
 end
+
+
+
+#If at any point you want to follow along with the example, you can put a binding.pry anywhere in the method and run ruby bin/my_find in your terminal to pry around.
